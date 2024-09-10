f6809f473f2fea5da6541ddefeba105eea734051
NikKessel <snaiwmarl@gmail.com>
2024-09-10 13:56:58 +0200

    Update config.py

--- lib/config.py
+++ lib/config.py
@@ -57,28 +57,105 @@ CONF.NYUCLASSES = [
     'shower curtain', 
     'toilet', 
     'sink', 
-    'otherprop'
+    'otherprop',
+    'book',               # New class
+    'trash can',           # New class
+    'box',                 # New class
+    'shelf',               # New class
+    'mirror',              # New class
+    'plant',               # New class
+    'whiteboard',          # New class
+    'keyboard',            # New class
+    'tv',                  # New class
+    'computer tower',      # New class
+    'telephone',           # New class
+    'refrigerator',        # Already exists
+    'microwave',           # New class
+    'laptop',              # New class
+    'printer',             # New class
+    'soap dispenser',      # New class
+    'light',               # New class
+    'fan',                 # New class
+    'ceiling light',       # New class
+    'clock',               # New class
+    'rail',                # New class
+    'bulletin board',      # New class
+    'trash bin',           # New class
+    'mouse',               # New class
+    'person',              # New class
+    'fire extinguisher',   # New class
+    'ladder',              # New class
+    'pipe',                # New class
+    'bookshelf',           # Already exists
+    'projector screen',    # New class
+    'fire alarm',          # New class
+    'projector',           # New class
+    'smoke detector',      # New class
+    'heater',              # New class
+    'scanner',             # New class
+    'stair',               # New class
+    'car'                  # New class
 ]
+
+# Update the number of classes based on the new list
 CONF.NUM_CLASSES = len(CONF.NYUCLASSES)
+
+# Update the palette with new colors for new classes
+# You can customize these colors to your preference, but I'm adding placeholder RGB values here
 CONF.PALETTE = [
-    (152, 223, 138),		# floor
-    (174, 199, 232),		# wall
-    (31, 119, 180), 		# cabinet
-    (255, 187, 120),		# bed
-    (188, 189, 34), 		# chair
-    (140, 86, 75),  		# sofa
-    (255, 152, 150),		# table
-    (214, 39, 40),  		# door
-    (197, 176, 213),		# window
-    (148, 103, 189),		# bookshelf
-    (196, 156, 148),		# picture
-    (23, 190, 207), 		# counter
-    (247, 182, 210),		# desk
-    (219, 219, 141),		# curtain
-    (255, 127, 14), 		# refrigerator
-    (227, 119, 194),		# bathtub
-    (158, 218, 229),		# shower curtain
-    (44, 160, 44),  		# toilet
-    (112, 128, 144),		# sink
+    (152, 223, 138),        # floor
+    (174, 199, 232),        # wall
+    (31, 119, 180),         # cabinet
+    (255, 187, 120),        # bed
+    (188, 189, 34),         # chair
+    (140, 86, 75),          # sofa
+    (255, 152, 150),        # table
+    (214, 39, 40),          # door
+    (197, 176, 213),        # window
+    (148, 103, 189),        # bookshelf
+    (196, 156, 148),        # picture
+    (23, 190, 207),         # counter
+    (247, 182, 210),        # desk
+    (219, 219, 141),        # curtain
+    (255, 127, 14),         # refrigerator
+    (227, 119, 194),        # bathtub
+    (158, 218, 229),        # shower curtain
+    (44, 160, 44),          # toilet
+    (112, 128, 144),        # sink
     (82, 84, 163),          # otherfurn
+    (60, 180, 75),          # book (new color)
+    (0, 128, 128),          # trash can (new color)
+    (255, 99, 71),          # box (new color)
+    (238, 130, 238),        # shelf (new color)
+    (127, 255, 0),          # mirror (new color)
+    (173, 216, 230),        # plant (new color)
+    (245, 222, 179),        # whiteboard (new color)
+    (255, 228, 181),        # keyboard (new color)
+    (255, 255, 0),          # tv (new color)
+    (139, 69, 19),          # computer tower (new color)
+    (218, 165, 32),         # telephone (new color)
+    (255, 127, 80),         # microwave (new color)
+    (0, 255, 255),          # laptop (new color)
+    (255, 215, 0),          # printer (new color)
+    (123, 104, 238),        # soap dispenser (new color)
+    (72, 209, 204),         # light (new color)
+    (139, 0, 139),          # fan (new color)
+    (255, 140, 0),          # ceiling light (new color)
+    (65, 105, 225),         # clock (new color)
+    (255, 69, 0),           # rail (new color)
+    (50, 205, 50),          # bulletin board (new color)
+    (199, 21, 133),         # trash bin (new color)
+    (255, 20, 147),         # mouse (new color)
+    (72, 61, 139),          # person (new color)
+    (255, 165, 0),          # fire extinguisher (new color)
+    (32, 178, 170),         # ladder (new color)
+    (47, 79, 79),           # pipe (new color)
+    (127, 255, 212),        # projector screen (new color)
+    (189, 183, 107),        # fire alarm (new color)
+    (144, 238, 144),        # projector (new color)
+    (255, 69, 0),           # smoke detector (new color)
+    (139, 0, 0),            # heater (new color)
+    (255, 20, 147),         # scanner (new color)
+    (0, 0, 128),            # stair (new color)
+    (135, 206, 250)         # car (new color)
 ]
